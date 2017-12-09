63ca25c0775871e8155b50b749656bf1f978c11b
Giel De Clercq <giel.de.clercq@gmail.com>
2017-12-09 17:42:04 +0100

    ViewModelBase aangemaakt en Profiel hernoemt naar Gebruiker

--- WishlistApp/Models/Gebruiker.cs
+++ WishlistApp/Models/Gebruiker.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WishlistApp.Models
 {
-    class Profiel
+    class Gebruiker
     {
     }
 }
---
+++ WishlistApp/Viewmodels/ViewModelBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace WishlistApp.Viewmodels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        private DataTemplate _template;
+
+        public DataTemplate Template
+        {
+            get { return _template; }
+            set
+            {
+                _template = value;
+                RaisePropertyChanged("Template");
+            }
+        }
+
+        public ViewModelBase()
+        {
+            Template = GetTemplate();
+        }
+
+        private DataTemplate GetTemplate()
+        {
+            string s = GetType().Name;
+            return (DataTemplate)App.Current.Resources[s];
+
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
