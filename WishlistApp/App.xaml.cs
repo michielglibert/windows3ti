e9373adc6407dc09edfdf5c5e738fa37da2bca76
Giel De Clercq <giel.de.clercq@gmail.com>
2017-12-09 17:07:35 +0100

    Klaarzetten MVVM

--- WishlistApp/App.xaml.cs
+++ WishlistApp/App.xaml.cs
@@ -66,7 +66,7 @@ namespace WishlistApp
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(Profiel), e.Arguments);
+                    rootFrame.Navigate(typeof(Views.Profiel), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
